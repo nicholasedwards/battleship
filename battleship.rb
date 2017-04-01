a5f18863a78850efb8a9b61cf48e66aee84804ba
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-03-31 19:08:34 -0600

    REPL for accepting user input and outputting text

---
+++ battleship.rb
@@ -0,0 +1,20 @@
+user_input_provided = false
+
+until user_input_provided
+  puts "Welcome to BATTLESHIP"
+  puts ""
+  puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+  puts ">"
+
+  action = gets.chomp
+    if action == "p" || action == "play"
+      # play the game!
+      user_input_provided = true
+    elsif action == "i" || action == "instructions"
+      instructions = File.open("./instructions/instructions.txt", "r")
+      puts instructions.read
+    elsif action == "q" || action == "quit"
+      puts "Goodbye!"
+      user_input_provided = true 
+    end
+end
