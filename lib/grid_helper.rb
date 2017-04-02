75c8216840fde0c57b2fdaf684a19671b02439f1
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-02 10:39:12 -0600

    Extract grid helper logic to a module

--- lib/grid_helper.rb
+++ lib/grid_helper.rb
@@ -1,30 +1,30 @@
+module GridHelper
+    def self.format_position(input_position)
+    letters = input_position.split("")
+
+    row_index = 0 if letters[0] == "A"
+    row_index = 1 if letters[0] == "B"
+    row_index = 2 if letters[0] == "C"
+    row_index = 3 if letters[0] == "D"
+
+    column_index = 0 if letters[1] == "1"
+    column_index = 1 if letters[1] == "2"
+    column_index = 2 if letters[1] == "3"
+    column_index = 3 if letters[1] == "4"
+
+    [row_index, column_index]
+  end
+end
+
 class Array
+include GridHelper
+
   def place_small_ship(start_position, end_position)
-    start_row_letter = start_position.split("")[0]
-    start_row_index = 0 if start_row_letter == "A"
-    start_row_index = 1 if start_row_letter == "B"
-    start_row_index = 2 if start_row_letter == "C"
-    start_row_index = 3 if start_row_letter == "D"
-
-    start_column_number = start_position.split("")[1]
-    start_column_index = 0 if start_column_number == "1"
-    start_column_index = 1 if start_column_number == "2"
-    start_column_index = 2 if start_column_number == "3"
-    start_column_index = 3 if start_column_number == "4"
-
-    end_row_letter = end_position.split("")[0]
-    end_row_index = 0 if end_row_letter == "A"
-    end_row_index = 1 if end_row_letter == "B"
-    end_row_index = 2 if end_row_letter == "C"
-    end_row_index = 3 if end_row_letter == "D"
-
-    end_column_number = end_position.split("")[1]
-    end_column_index = 0 if end_column_number == "1"
-    end_column_index = 1 if end_column_number == "2"
-    end_column_index = 2 if end_column_number == "3"
-    end_column_index = 3 if end_column_number == "4"
+    start_row_index, start_column_index = GridHelper::format_position(start_position)
+    end_row_index, end_column_index = GridHelper::format_position(end_position)
 
     self[start_row_index][start_column_index].is_ship = true
     self[end_row_index][end_column_index].is_ship = true
   end
+
 end
