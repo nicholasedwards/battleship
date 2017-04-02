5b5091cb538ae714dac3ddfcddb62fe6589f7e9e
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-02 11:19:37 -0600

    Implement validation of small ship length

--- lib/grid_helper.rb
+++ lib/grid_helper.rb
@@ -22,7 +22,11 @@ include GridHelper
   def place_small_ship(start_position, end_position)
     start_row_index, start_column_index = GridHelper::format_position(start_position)
     end_row_index, end_column_index = GridHelper::format_position(end_position)
+    if ( end_column_index - start_column_index ) > 1 || ( end_row_index - start_row_index ) > 1
+      puts "You tried placing a ship greater than two cells long! Please try again."
+    else
     set_ends_of_ship(start_row_index, start_column_index, end_row_index, end_column_index)
+    end
   end
 
   def place_large_ship(start_position, end_position)
