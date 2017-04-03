aa05cf6deba909bb811d61d545ebfbb3a54c15fb
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-03 15:20:47 -0600

    Set up GamePlay::display_grid to take a grid as an argument

--- test/gameplay_test.rb
+++ test/gameplay_test.rb
@@ -4,14 +4,20 @@ include GamePlay
 
 class TestGamePlay < Minitest::Test
   def test_display_grid
+    empty_grid = Grid.new
+    grid = empty_grid.populate_grid
+    grid_with_computer_ships = ShipPlacement::place_computer_ships(grid)
     assert_output(/false/) { 
-      GamePlay::display_grid
+      GamePlay::display_grid(grid_with_computer_ships)
     }
   end
 
   def test_display_grid_headers
+    empty_grid = Grid.new
+    grid = empty_grid.populate_grid
+    grid_with_computer_ships = ShipPlacement::place_computer_ships(grid)
     assert_output(/A/) { 
-      GamePlay::display_grid
+      GamePlay::display_grid(grid_with_computer_ships)
     }
   end
 end
