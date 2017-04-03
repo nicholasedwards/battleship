8110e06b44468e0a4af8e5c13e0e8b576d884410
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-03 11:57:38 -0600

    Computer large ship placement works sometimes
    There is still an occasionally failing edge case that I haven't figured out yet

--- lib/ship_placement_helper.rb
+++ lib/ship_placement_helper.rb
@@ -3,7 +3,8 @@ require './lib/grid_helper'
 module ShipPlacement
   def place_computer_ships(grid)
     grid_with_small_ship = place_computer_small_ship(grid)
-    grid_with_small_ship
+    grid_with_both_ships = place_computer_large_ship(grid_with_small_ship)
+    grid_with_both_ships
   end
 
   def place_computer_small_ship(grid)
@@ -13,6 +14,20 @@ module ShipPlacement
       end_row_index, end_column_index, end_cell = generate_small_ship_end_position(start_row_index, start_column_index)
       if !grid[end_row_index].nil? && !grid[end_row_index][end_column_index].nil?
         grid.place_small_ship(start_cell, end_cell)
+        ship_placed = true if grid[end_row_index][end_column_index].is_ship? == true
+      end
+    end
+    grid
+  end
+
+  def place_computer_large_ship(grid)
+    ship_placed = false
+    until ship_placed
+      start_row_index, start_column_index, start_cell = generate_start_position
+      next if grid[start_row_index][start_column_index].is_ship? == true
+      end_row_index, end_column_index, end_cell = generate_large_ship_end_position(start_row_index, start_column_index)
+      if !grid[end_row_index].nil? && !grid[end_row_index][end_column_index].nil?
+        grid.place_large_ship(start_cell, end_cell)
         ship_placed = true if grid[end_row_index][end_column_index].is_ship? == true
       end
     end
@@ -38,6 +53,21 @@ module ShipPlacement
       end_row_index, end_column_index = set_small_ship_end_below(start_row_index, start_column_index)
     elsif direction == :left
       end_row_index, end_column_index = set_small_ship_end_left(start_row_index, start_column_index)
+    end
+    end_cell = set_formatted_end_cell(end_row_index, end_column_index)
+    [end_row_index, end_column_index, end_cell]
+  end
+
+  def generate_large_ship_end_position(start_row_index, start_column_index)
+    direction = [:above, :right, :below, :left].sample
+    if direction == :above
+      end_row_index, end_column_index = set_large_ship_end_above(start_row_index, start_column_index)
+    elsif direction == :right
+      end_row_index, end_column_index = set_large_ship_end_right(start_row_index, start_column_index)
+    elsif direction == :below
+      end_row_index, end_column_index = set_large_ship_end_below(start_row_index, start_column_index)
+    elsif direction == :left
+      end_row_index, end_column_index = set_large_ship_end_left(start_row_index, start_column_index)
     end
     end_cell = set_formatted_end_cell(end_row_index, end_column_index)
     [end_row_index, end_column_index, end_cell]
@@ -86,6 +116,52 @@ module ShipPlacement
     end
    [end_row_index, end_column_index]
   end
+
+  def set_large_ship_end_above(start_row_index, start_column_index)
+    end_column_index = start_column_index
+    if start_row_index == 0 || start_row_index == 1
+      # at the edge of the grid. switch direction so as not to go off the grid.
+      end_row_index = start_row_index + 2
+    else
+      end_row_index = start_row_index - 2
+    end
+    [end_row_index, end_column_index]
+  end
+
+  def set_large_ship_end_right(start_row_index, start_column_index)
+    end_row_index = start_row_index
+    if start_column_index == 3 || start_column_index == 2
+      # at the edge of the grid. switch direction so as not to go off the grid.
+      end_column_index = start_column_index - 2
+    else
+      end_column_index = start_column_index + 2
+    end
+    [end_row_index, end_column_index]
+  end
+
+  def set_large_ship_end_below(start_row_index, start_column_index)
+    end_column_index = start_column_index
+    if start_row_index == 3 || start_row_index == 2
+      # at the edge of the grid. switch direction so as not to go off the grid.
+      end_row_index = start_row_index - 2
+    else
+      end_row_index = start_row_index + 2
+    end
+    [end_row_index, end_column_index]
+  end
+
+  def set_large_ship_end_left(start_row_index, start_column_index)
+    end_row_index = start_row_index
+    if start_column_index == 0 || start_column_index == 1
+      # at the edge of the grid. switch direction so as not to go off the grid.
+      end_column_index = start_column_index + 2
+    else
+      end_column_index = start_column_index - 2
+    end
+   [end_row_index, end_column_index]
+  end
+
+
 
   def set_formatted_end_cell(end_row_index, end_column_index)
     end_row_letter = {0 => "A", 1 => "B", 2 => "C", 3 => "D"}[end_row_index]
