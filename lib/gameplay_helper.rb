ab7c5530b932f8e3a409dd9830884b5585cb8818
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-03 15:12:36 -0600

    Implement grid headers

--- lib/gameplay_helper.rb
+++ lib/gameplay_helper.rb
@@ -8,8 +8,12 @@ module GamePlay
     empty_grid = Grid.new
     grid = empty_grid.populate_grid
     grid_with_computer_ships = ShipPlacement::place_computer_ships(grid)
+    puts "  1  2  3  4"
     4.times do |row_index|
+      row_letter = { 0 => "A", 1 => "B", 2 => "C", 3 => "D"}[row_index]
+      print row_letter
       4.times do |column_index|
+        print " "
         print ( grid_with_computer_ships[row_index][column_index].is_ship? == true )
         print " "
       end
