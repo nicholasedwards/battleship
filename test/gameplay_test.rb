e28356b5e886c4e6346a3c5ae59c2f02cb7dd11e
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-03 09:02:19 -0600

    Test that the computer places its own ships

---
+++ test/gameplay_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require './lib/grid'
+require './lib/gameplay_helper'
+include GamePlay
+
+class TestGamePlay < Minitest::Test
+
+  def test_computer_places_ships
+    empty_grid = Grid.new
+    grid = empty_grid.populate_grid
+    grid_with_computer_ships = GamePlay::place_computer_ships(grid)
+
+    ship_cells = 0
+    grid_with_computer_ships.each do |row|
+      row.each do |cell|
+        ship_cells += 1 if cell.is_ship? == true
+      end
+    end
+    
+    assert_equal 5, ship_cells
+  end
+
+end
