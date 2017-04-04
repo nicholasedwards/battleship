00b1f5bcfeacdd378d35d7ef37e1ec72abdbeaaa
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-04 14:36:39 -0600

    Test that a player can shoot
    When a player shoots a cell containing a ship, that cell's status is set to 'H'. When a player shoots a cell not containing a ship, that cell's status is set to 'M'.

---
+++ test/player_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+
+class TestPlayer < Minitest::Test
+  def test_player_can_shoot
+    empty_ai_grid = Grid.new
+    ai_grid = empty_ai_grid.populate_grid
+    ai_grid[0][0].is_ship? = true
+    ai_grid[0][1].is_ship? = false
+    player = Player.new
+    player.shoot("A1", ai_grid)
+    player.shoot("A2", ai_grid)
+    assert_equal "H", ai_grid[0][0].status
+    assert_equal "M", ai_grid[0][1].status
+  end
+end
