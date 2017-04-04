3e1e2584baf4a80152749055dd4feab7929117ca
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-04 14:56:06 -0600

    Player can fire on a target cell

---
+++ lib/player.rb
@@ -0,0 +1,14 @@
+require './lib/formatting_helper'
+include Formatting
+
+class Player
+  def fire(target, grid)
+    row_index, column_index = format_position(target)
+    if grid[row_index][column_index].is_ship? == true
+      grid[row_index][column_index].status = "H"
+    else
+      grid[row_index][column_index].status = "M"
+    end
+    grid
+  end
+end
--- test/player_test.rb
+++ test/player_test.rb
@@ -6,11 +6,11 @@ class TestPlayer < Minitest::Test
   def test_player_can_shoot
     empty_ai_grid = Grid.new
     ai_grid = empty_ai_grid.populate_grid
-    ai_grid[0][0].is_ship? = true
-    ai_grid[0][1].is_ship? = false
+    ai_grid[0][0].is_ship = true
+    ai_grid[0][1].is_ship = false
     player = Player.new
-    player.shoot("A1", ai_grid)
-    player.shoot("A2", ai_grid)
+    player.fire("A1", ai_grid)
+    player.fire("A2", ai_grid)
     assert_equal "H", ai_grid[0][0].status
     assert_equal "M", ai_grid[0][1].status
   end
