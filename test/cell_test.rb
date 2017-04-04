84a19ab2ae47b96e01f631a2103cbf56c16c0d24
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-04 14:44:51 -0600

    Test for cell status attribute

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -7,4 +7,14 @@ class TestCell < Minitest::Test
     assert_equal 0, new_cell.row_index
     assert_equal 1, new_cell.column_index
   end
+
+  def test_cell_is_either_a_ship_or_not
+    new_cell = Cell.new(0,1)
+    assert_respond_to(new_cell, :is_ship?)
+  end
+
+  def test_cell_has_a_status
+    new_cell = Cell.new(0,1)
+    assert_respond_to(new_cell, :status)
+  end
 end
