4b612ea0299db841396b8cc2ceb0399e94b16e0b
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-04 14:46:42 -0600

    Implement cell status attribute

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,13 +1,13 @@
 class Cell
   attr_reader :row_index, :column_index
-  attr_accessor :is_ship
+  attr_accessor :is_ship, :status
 
   def initialize(row_index, column_index)
     @row_index = row_index
     @column_index = column_index
     @is_ship = false
   end
-
+  
   def is_ship?
     @is_ship
   end
