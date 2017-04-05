7312222666994160c6ecdc0bd9761dd0f22d668d
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-05 17:07:57 -0600

    Test that AI's score increments when it hits a player's ship
    Already green

--- test/ai_test.rb
+++ test/ai_test.rb
@@ -20,4 +20,20 @@ class TestAI < Minitest::Test
     
     assert_equal "H", player_grid[row_index][column_index].status
   end
+
+  def test_ai_score
+    empty_player_grid = Grid.new
+    player_grid = empty_player_grid.populate_grid
+
+    4.times do |row_index|
+      4.times do |column_index|
+        player_grid[row_index][column_index].is_ship = true
+      end
+    end
+
+    ai = AI.new
+    row_index, column_index, player_grid = ai.fire(player_grid)
+
+    assert_equal 1, ai.score
+  end
 end
