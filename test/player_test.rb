03dd2b761a391c6ced15625a0d837edd4c079d7a
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-04-05 16:54:20 -0600

    Test that the player can win the game

--- test/player_test.rb
+++ test/player_test.rb
@@ -14,4 +14,22 @@ class TestPlayer < Minitest::Test
     assert_equal "H", ai_grid[0][0].status
     assert_equal "M", ai_grid[0][1].status
   end
+
+  def test_player_wins
+    empty_ai_grid = Grid.new
+    ai_grid = empty_ai_grid.populate_grid
+    ai_grid[0][0].is_ship = true
+    ai_grid[0][1].is_ship = true
+    ai_grid[1][0].is_ship = true
+    ai_grid[1][1].is_ship = true
+    ai_grid[1][2].is_ship = true
+    player.fire("A1", ai_grid)
+    player.fire("A2", ai_grid)
+    player.fire("B1", ai_grid)
+    player.fire("B2", ai_grid)
+    
+    assert_output(/You sunk all of the AI's ships! Nice work, sailor.\n/) {
+      player.fire("B3", ai_grid)
+    }
+  end
 end
